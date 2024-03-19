90a6901db6ffaba6e227e4cf717a00ff0d22a39c
Saksham Mathur <60032069+sammathur4@users.noreply.github.com>
2024-03-19 15:06:43 +0530

    Time: 358 ms (96.43%) | Memory: 16.6 MB (92.40%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.py
+++ 1250-longest-common-subsequence/longest-common-subsequence.py
@@ -1,16 +1,15 @@
 class Solution:
-    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        len_text1, len_text2 = len(text1), len(text2)
-
-        dp_matrix = [
-            [0]*(len_text2+1) for _ in range(len_text1+1)
-        ]
-
-        for i in range(1, len_text1+1):
-            for j in range(1, len_text2+1):
-                if text1[i-1] == text2[j-1]:
-                    dp_matrix[i][j] = dp_matrix[i-1][j-1] + 1
+    def longestCommonSubsequence(self, text1, text2):
+        if len(text2) < len(text1):
+            text1, text2 = text2, text1
+        previous = [0] * (len(text1) + 1)
+        current = [0] * (len(text1) + 1)
+        for col in reversed(range(len(text2))):
+            for row in reversed(range(len(text1))):
+                if text2[col] == text1[row]:
+                    current[row] = 1 + previous[row + 1]
                 else:
-                    dp_matrix[i][j] = max(dp_matrix[i-1][j], dp_matrix[i][j-1])
-
-        return dp_matrix[len_text1][len_text2]        
+                    current[row] = max(previous[row], current[row + 1])
+            previous, current = current, previous
+        
+        return previous[0]
