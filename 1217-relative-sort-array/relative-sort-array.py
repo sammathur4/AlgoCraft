9cff6f2b35e985b393f6d183f87750bfc07b4d3f
Saksham Mathur <60032069+sammathur4@users.noreply.github.com>
2024-06-11 15:06:35 +0530

    Time: 43 ms (43.51%) | Memory: 16.6 MB (67.06%) - LeetSync

---
+++ 1217-relative-sort-array/relative-sort-array.py
@@ -0,0 +1,11 @@
+class Solution:
+    def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
+        len_arr2 = len(arr2)
+        a2i = [len_arr2]*1001
+
+        for i, x in enumerate(arr2):
+            a2i[x] = i
+
+        arr1.sort(key=lambda x:(a2i[x], x))
+        return arr1
+        
